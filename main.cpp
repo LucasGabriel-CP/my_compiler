89632901602c707b8be5098e73fe111fa0ac3c15
LucasGabriel-CP <lucasgabrielgarriel@hotmail.com>
2023-04-12 02:02:51 -0300

    teste

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
     std::ofstream outFile("output.txt");
     while (tk.get_type() != "NULL") {
         if (tk.get_type() != "ERROR") {
-            outFile << tk << " \n"[tk.get_type() == ";"];adasdasd
+            outFile << tk << " \n"[tk.get_type() == ";"];
         }
         tk = lx.next_token();
     }
