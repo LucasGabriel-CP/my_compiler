84b9e920f1e1cff6602002844f8102f954db0b73
Lucas Gabriel <79482172+LucasGabriel-CP@users.noreply.github.com>
2023-04-14 14:22:32 -0300

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -10,8 +10,8 @@ parser::parser(std::unordered_map<std::string, int, custom_hash> h1,
                                         ")", "(", "{", "}", ",", ";", "int", "float", "string",
                                         "=", "opM", "opL", "$" };
     this->tokens = tokens;
-
-    //ConstruÁ„o da tabela
+    this->tokens.push_back(token("-1", "$", std::pair<int, int>(-1, -1)));
+    //Constru√ß√£o da tabela
     predictive_table["<termo>"] = { { "id", {"id"} }, {"constante", {"constante"}} };
     predictive_table["<bloco>"] = { {"id", {"<comando>", "<bloco>"}}, {"while", {"<comando>", "<bloco>"}}, {"if", {"<comando>", "<bloco>"}},
         {"read", {"<comando>", "<bloco>"}}, {"print", {"<comando>", "<bloco>"}},
@@ -31,10 +31,46 @@ parser::parser(std::unordered_map<std::string, int, custom_hash> h1,
     predictive_table["<read'>"] = { {")", {"empty"}}, {",", {",", "id", "<read'>"}} };
     predictive_table["<print>"] = { {"print", {"print", "(", "<termo>", "<print'>", ")", ";"}} };
     predictive_table["<print'>"] = { {")", {"empty"}}, {",", {",", "<termo>", "<print'>"}} };
-    predictive_table["decl"] = { {"int", {"int", "id", "<decl'>", ";"}}, {"float", {"float", "id", "<decl'>", ";"}}, {"string", {"string", "id", "<decl'>", ";"}} };
-    predictive_table["decl'"] = { {",", {",", "id", "<decl'>"}}, {";", {"empty"}} };
-    predictive_table["exp_atrib"] = { {"id", {"id", "=", "<exp>", ";"}} };
-    predictive_table["exp"] = { {"id", {"<termo>", "<exp'>"}}, {"constante", {"<termo>", "<exp'>"}} };
-    predictive_table["exp'"] = { {";", {"empty"}}, {"opM", {"opM", "<termo>", "<exp'>"}}, {"opL", {"opL", "<termo>"}} };
+    predictive_table["<decl>"] = { {"int", {"int", "id", "<decl'>", ";"}}, {"float", {"float", "id", "<decl'>", ";"}}, {"string", {"string", "id", "<decl'>", ";"}} };
+    predictive_table["<decl'>"] = { {",", {",", "id", "<decl'>"}}, {";", {"empty"}} };
+    predictive_table["<exp_atrib>"] = { {"id", {"id", "=", "<exp>", ";"}} };
+    predictive_table["<exp>"] = { {"id", {"<termo>", "<exp'>"}}, {"constante", {"<termo>", "<exp'>"}} };
+    predictive_table["<exp'>"] = { {";", {"empty"}}, {"opM", {"opM", "<termo>", "<exp'>"}}, {"opL", {"opL", "<termo>"}} };
 }
 
+void parser::work(){
+	std::stack<std::string> st;
+	st.push("$");
+	st.push("<bloco>");
+	int id = 0;
+	while(!st.empty()){
+		std::string at = st.top(); st.pop();
+		if (at == tokens[id].get_type()){
+			std::cout << tokens[id] << '\n';
+			id++;
+			if (at == "$"){
+				continue;
+			}
+			//terminal com terminal
+		}
+		else if (at[0] != '<'){
+			if (at == "empty") continue;
+			//erro analise sintatica
+			std::cout << "Missing terminal for " << at << '\n';
+			id++;
+		}
+		else{
+			std::string aux = tokens[id].get_type();
+			if (aux == "FNumber" || aux == "INumber" || aux == "Frase"){
+				aux = "constante";
+			}
+			if (predictive_table[at][aux].empty()){
+				std::cout << "deu merda na analise\n";
+				continue;
+			}
+			for (int i = (int)predictive_table[at][aux].size() - 1; i >= 0; i--){
+				st.push(predictive_table[at][aux][i]);
+			}
+		}
+	}
+}
