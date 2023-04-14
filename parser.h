60f6c303e6d5c6f95f0cbb668fe45f6ea32beb11
Lucas Gabriel <79482172+LucasGabriel-CP@users.noreply.github.com>
2023-04-14 14:23:47 -0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -9,11 +9,13 @@
 #include<fstream>
 #include<chrono>
 #include<random>
-#include"token.h"
+#include<stack>
+#include "token.h"
+#include "lexer.h"
 
-class parser{
+class parser : public lexer{
 private:
-    struct custom_hash {
+    /*struct custom_hash {
         static uint64_t splitmix64(uint64_t x) {
             x += 0x9e3779b97f4a7c15;
             x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
@@ -30,7 +32,7 @@ private:
             static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
             return splitmix64(std::hash<std::string>()(x) + FIXED_RANDOM);
         }
-    };
+    };*/
     std::unordered_map<std::string,
             std::unordered_map<std::string, std::vector<std::string>, custom_hash>, custom_hash> predictive_table;
     std::unordered_map<std::string, int, custom_hash> hash_by_word;
@@ -40,6 +42,5 @@ private:
 public:
     parser();
     parser(std::unordered_map<std::string, int, custom_hash> h1, std::unordered_map<int, std::string, custom_hash> h2, std::vector<token> const &tokens);
-
+	void work();
 };
-
